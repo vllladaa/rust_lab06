f6bf04136acaf89f8293aeab1cda9bf7a8a66d90
vlada <krivutsavv@gmail.com>
2024-11-11 22:00:31 +0200

    added code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+fn gcd(mut a: u32, mut b: u32) -> u32 {
+    while b != 0 {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    a
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_gcd() {
+        let data = [
+            ((24,  60), 12),
+            ((15,   9),  3),
+            ((15,   6),  3),
+            ((140, 40), 20),
+            ((24,  16),  8),
+            ((100, 10), 10),
+            ((120, 80), 40),
+            ((80, 120), 40),
+            ((100, 20), 20),
+            ((37,  11),  1),
+            ((120, 90), 30),
+        ];
+
+        for ((a, b), exp) in data.iter() {
+            assert_eq!(*exp, gcd(*a, *b));
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = 24;
+    let b = 60;
+    println!("GCD of {} and {} is {}", a, b, gcd(a, b));
 }
